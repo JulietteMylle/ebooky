6ae36d5da99a0672cbe546681d8ab5b5134d06b1
Raniouu <R.jay@live.fr>
2024-04-15 16:26:52 +0200

    BarProfile avec page profile et register

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -1,90 +1,134 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Pen } from "lucide-react";
+import { Button } from "../components/ui/button";
+import { BarProfile } from "../components/organisms/BarProfile";
 
 function Profile() {
-    const [userData, setUserData] = useState(null);
-    const [newUserData, setNewUserData] = useState({
-        username: '',
-        email: ''
-    });
+  const [userData, setUserData] = useState(null);
+  const [newUserData, setNewUserData] = useState({
+    username: "",
+    email: "",
+  });
 
-    useEffect(() => {
-        const token = localStorage.getItem("session");
-        const parsedTokenObject = JSON.parse(token);
-        const tokenValue = parsedTokenObject.token;
+  useEffect(() => {
+    const token = localStorage.getItem("session");
+    const parsedTokenObject = JSON.parse(token);
+    const tokenValue = parsedTokenObject.token;
 
-        axios.get('https://localhost:8000/profile', {
-            headers: { 'Authorization': 'Bearer ' + tokenValue }
-        })
-            .then(response => {
-                setUserData(response.data);
-                setNewUserData(response.data); // Assigner les données utilisateur existantes à newUserData lorsqu'elles sont disponibles
-            })
-            .catch(error => {
-                console.error("Error fetching profile:", error);
-            });
-    }, []);
+    axios
+      .get("https://localhost:8000/profile", {
+        headers: { Authorization: "Bearer " + tokenValue },
+      })
+      .then((response) => {
+        setUserData(response.data);
+        setNewUserData(response.data); // Assigner les données utilisateur existantes à newUserData lorsqu'elles sont disponibles
+      })
+      .catch((error) => {
+        console.error("Error fetching profile:", error);
+      });
+  }, []);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setNewUserData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewUserData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const token = localStorage.getItem("session");
-        const parsedTokenObject = JSON.parse(token);
-        const tokenValue = parsedTokenObject.token;
+    const token = localStorage.getItem("session");
+    const parsedTokenObject = JSON.parse(token);
+    const tokenValue = parsedTokenObject.token;
 
-        try {
-            await axios.put('https://localhost:8000/updateProfile', newUserData, {
-                headers: { 'Authorization': 'Bearer ' + tokenValue }
-            });
-            alert('Profile updated successfully!');
-            setUserData(newUserData);
-        } catch (error) {
-            console.error("Error updating profile:", error);
-            alert('An error occurred while updating profile. Please try again.');
-        }
-    };
+    try {
+      await axios.put("https://localhost:8000/updateProfile", newUserData, {
+        headers: { Authorization: "Bearer " + tokenValue },
+      });
+      alert("Profile updated successfully!");
+      setUserData(newUserData);
+    } catch (error) {
+      console.error("Error updating profile:", error);
+      alert("An error occurred while updating profile. Please try again.");
+    }
+  };
 
-    const handleDisconnect = async (e) => {
-        e.preventDefault();
+  const handleDisconnect = async (e) => {
+    e.preventDefault();
 
+    localStorage.removeItem("session");
+    // ici pour rediriger vers page acc
+    window.location.href = "/";
+    // Rediriger l'utilisateur vers une page de confirmation ou de déconnexion
+  };
 
-        localStorage.removeItem("session");
-        // ici pour rediriger vers page acc 
-        window.location.href = "/";
-        // Rediriger l'utilisateur vers une page de confirmation ou de déconnexion
-    };
+  return (
+    <div>
+      <BarProfile userData={userData} />
 
-    return (
-        <div>
-            <p>Mon profil</p>
+      <div className="flex justify-center">
+        <div className="border p-12 rounded-3xl m-30 mt-12 mb-12 bg-[url('src/assets/images/cover_img.png')] drop-shadow">
+          <div>
+            <p>Mon Profil</p>
+          </div>
 
-            {userData && (
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label>Nom d'utilisateur:</label>
-                        <input type="text" name="username" value={newUserData.username} onChange={handleChange} />
-                    </div>
-                    <div>
-                        <label>Email:</label>
-                        <input type="email" name="email" value={newUserData.email} onChange={handleChange} />
-                    </div>
-                    <button type="submit">Mettre à jour le profil</button>
-                    <button onClick={handleDisconnect}> Se déconnecter </button>
+          {userData && (
+            <form
+              className="text-center bg-[#F2F7F3] rounded-lg"
+              onSubmit={handleSubmit}
+            >
+              <div className=" my-20 text-xl">
+                <label>Nom d'utilisateur:</label>
+                <input
+                  type="text"
+                  name="username"
+                  value={newUserData.username}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="text-center my-4 text-xl">
+                <label>Email:</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={newUserData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col place-content-center">
+                <div className="justify-center m-4">
+                  <Button
+                    type="submit"
+                    className="flex bg-[#064e3b] mx-40 text-white py-2 px-20 rounded-full size-10"
+                  >
+                    <Pen className="" /> Modifier
+                  </Button>
+                </div>
+                <div className="m-4">
+                  <Button
+                    className="flex bg-[#064e3b] mx-40 text-white py-2 px-20 rounded-full size-10"
+                    onClick={handleDisconnect}
+                  >
+                    {" "}
+                    Se déconnecter{" "}
+                  </Button>
+                </div>
+                <div className="justify-center m-4">
+                  <Button className="flex bg-[#064e3b] mx-40 text-white py-2 px-20 rounded-full size-10">
                     <Link to="/deleteProfile">Supprimer mon compte</Link>
-                </form>
-            )
-            }
-        </div >
-    );
+                  </Button>
+                </div>
+              </div>
+            </form>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
