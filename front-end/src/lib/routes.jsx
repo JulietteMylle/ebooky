60e42c044e5e06fb48d46a2704e29c0620f4eaac
Raniouu <R.jay@live.fr>
2024-04-15 10:32:42 +0200

    ErrorPage

--- front-end/src/lib/routes.jsx
+++ front-end/src/lib/routes.jsx
@@ -4,21 +4,19 @@ import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
 import DeleteProfile from "../pages/DeleteProfile";
+import ErrorPage from "../pages/ErrorPage";
 
 function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/deleteProfile" element={<DeleteProfile />} />
-
-
-
-
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/deleteProfile" element={<DeleteProfile />} />
+      <Route path="/errorPage" element={<ErrorPage />} />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
--- front-end/src/pages/ErrorPage.jsx
+++ front-end/src/pages/ErrorPage.jsx
@@ -1,13 +1,24 @@
-export const ErrorPage = () => {
-    const returnBack = () => {
-        window.history.back();
-    };
-    return (
-        <div>
-            <img src="src/assets/Oups.png" alt="Oups" />
-            <h1>Page introuvable</h1>
-            <p>La page que vous recherchez n&apos;existe pas ou a été retirée</p>
-            <button onClick={returnBack}>Revenir en arrière</button>
-        </div>
-    );
+import { Button } from "../components/ui/button";
+
+const ErrorPage = () => {
+  const returnBack = () => {
+    window.history.back();
+  };
+
+  return (
+    <div
+      className="min-h-screen bg-cover bg-center text-[#064e3b]"
+      style={{ backgroundImage: "url('src/assets/images/Oups.png')" }}
+    >
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-4xl font-bold mb-4">Page introuvable</h1>
+        <p className="text-lg mb-4">
+          La page que vous recherchez n&apos;existe pas ou a été retirée.
+        </p>
+        <Button onClick={returnBack}>Revenir en arrière</Button>
+      </div>
+    </div>
+  );
 };
+
+export default ErrorPage;
