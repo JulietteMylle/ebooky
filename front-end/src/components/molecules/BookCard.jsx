2f4b49f21508cca29cd744cb20f9c92e83e3d5c8
Raniouu <R.jay@live.fr>
2024-04-17 11:08:42 +0200

    Front des pages modifiées

--- front-end/src/components/molecules/BookCard.jsx
+++ front-end/src/components/molecules/BookCard.jsx
@@ -1,19 +1,52 @@
-import { Button } from '../ui/button';
+// import { useEffect } from "react";
+import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function BookCard() {
-    return (
-        <div className="">
-            <div className=" flex flex-col ">
-                <img src="public/images/couverture-hp.png" className="w-60 h-80 flex flex-row pb-3" alt="" />
-                <div className="flex flex-row justify-between font-bold pb-1.5">
-                    <p>Nom du livre </p>
-                    <p>20$</p>
-                </div>
-                <div>
-                    <p className="text-sm">Auteur</p>
-                </div>
-                <Button className="m-5">Ajouter au panier</Button>
-            </div>
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://localhost:8000/books")
+      .then((response) => {
+        setBooks(response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des livres :", error);
+      });
+  }, []);
+
+  // return (
+  //   <div className="flex m-8">
+  //     <div className=" flex flex-col ">
+
+  //       <img
+  //         src="public/images/couverture-hp.png"
+  //         className="w-60 h-80 flex flex-row pb-3"
+  //         alt=""
+  //       />
+  //       <div className="flex flex-row justify-between font-bold pb-1.5">
+  //         <p>Nom du livre :{book.title}</p>
+  //         <p>20$</p>
+  //       </div>
+  //       <div>
+  //         <p className="text-sm">Auteur</p>
+  //       </div>
+  //       <Button className="m-5">Ajouter au panier</Button>
+  //     </div>
+  //   </div>
+  // );
+
+  return (
+    <div>
+      {books.map((book) => (
+        <div key={book.id}>
+          <div>{book.author}</div>
+          <h2>{book.title}</h2>
+          <p>Author: {book.author}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
--- front-end/src/components/organisms/NavBar.jsx
+++ front-end/src/components/organisms/NavBar.jsx
@@ -1,60 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { ChevronDown } from 'lucide-react';
-import Button from '../atoms/Button';
-import Logo from '../atoms/Logo';
-import SearchInput from '../atoms/SearchInput';
-import { ToggleTheme } from '../molecules/ToggleTheme';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { ChevronDown } from "lucide-react";
+import Button from "../atoms/Button";
+import Logo from "../atoms/Logo";
+import SearchInput from "../atoms/SearchInput";
+import { ToggleTheme } from "../molecules/ToggleTheme";
+import { Link } from "react-router-dom";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from '../ui/dropdown-menu';
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 const NavBar = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    useEffect(() => {
-        const token = localStorage.getItem("session");
-        if (token) {
-            setIsAuthenticated(true);
-        }
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("session");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
 
-    return (
-        <div className=" w-full flex items-center justify-between px-4 text-[#F2F7F3] bg-[#064e3b] dark:bg-[#26474E]">
-            <div className="w-14 h-12 ">
-                <Logo />
-            </div>
-            <DropdownMenu>
-                <DropdownMenuTrigger className="flex gap-1 items-center">
-                    Catégories <ChevronDown className="pt-1" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Profile</DropdownMenuItem>
-                    <DropdownMenuItem>Billing</DropdownMenuItem>
-                    <DropdownMenuItem>Team</DropdownMenuItem>
-                    <DropdownMenuItem>Subscription</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+  return (
+    <div className=" w-full flex items-center justify-between px-4 text-[#F2F7F3] bg-[#064e3b] dark:bg-[#26474E]">
+      <div className="w-14 h-12 ">
+        <Logo />
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="flex gap-1 items-center">
+          Catégories <ChevronDown className="pt-1" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-            <span>Mes recommandations</span>
-            <span>Ma bibliothèque</span>
-            <SearchInput />
-            <ToggleTheme />
-            {isAuthenticated ? (
-                <Link to="/profile">Mon Profil</Link>
-            ) : (
-                <Link to="/login">Se Connecter</Link>
-            )}
-            <Button content="Panier" icon="cart" />
-        </div>
-    );
+      <a href="/library">Librairie</a>
+
+      <span>Ma bibliothèque</span>
+      <SearchInput />
+      <ToggleTheme />
+      {isAuthenticated ? (
+        <Link to="/profile">Mon Profil</Link>
+      ) : (
+        <Link to="/login">Se Connecter</Link>
+      )}
+      <Button content="Panier" icon="cart" />
+    </div>
+  );
 };
 
 export default NavBar;
